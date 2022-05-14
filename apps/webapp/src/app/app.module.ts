42c7fe349f99f663f11bfccb746e725040ee8b8c
Imab Asghar <imab.asghar@spatialvision.com.au>
2022-05-14 21:10:33 +1000

    feat(webapp): Introduce routing in app and add home page and make it the default route

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
